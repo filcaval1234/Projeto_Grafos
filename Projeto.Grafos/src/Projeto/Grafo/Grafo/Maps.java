c5926329c11e69a74f8eef89a2ecd82c9d971ee0
filcaval1234 <felicavalcanti157@hotmail.com>
2017-08-22 21:34:58 -0300

    adicionando dijkstra

--- Projeto.Grafos/src/Projeto/Grafo/Grafo/Maps.java
+++ Projeto.Grafos/src/Projeto/Grafo/Grafo/Maps.java
@@ -4,8 +4,8 @@
  * and open the template in the editor.
  */
 package Projeto.Grafo.Grafo;
+import java.util.ArrayList;
 import java.util.Random;
-import java.util.stream.IntStream;
 /**
  *
  * @author fc.corporation
@@ -14,33 +14,81 @@ public class Maps {
     private int[][] adjacenciesMatrix;
     private Random weight;
     private Random coord;
-    public static final int[] LIMITEWEIGHT = {1,200};
+    private static final int[] LIMITEEDGE = {1,10};
+    private static final int LIMITEWEIGHT = 10;
+    private int size;
     
     public Maps(int tamanho){
-        adjacenciesMatrix = new int[tamanho-1][tamanho-1];
+        adjacenciesMatrix = new int[tamanho][tamanho];
         this.weight = new Random();
         this.coord = new Random();
+        this.size = tamanho;
+        this.setAdjacencieMatrix();
     }
     private void setAdjacencieMatrix(){
         for(int i=0;i < this.adjacenciesMatrix.length;i++){
-            
+            int j = 0;
+            int[] edges = this.coordGenerator(i);
+            while(j < edges.length){
+                if(edges[j] == 0)
+                    break;
+                if(i == j){
+                    j++;
+                    continue;
+                }
+                this.adjacenciesMatrix[i][edges[j]] = 1+this.weight.nextInt(
+                        this.LIMITEWEIGHT);
+                j++;
+            }
         }
     }
-    public int[] coordGenerator(){
-        int[] point = new int[2];
-        IntStream ints = weight.(0, 4);
-        System.err.println(weight.ints(0, 4));
-        for(int i: in){
-            System.out.println(i);
+    public int[] coordGenerator(int linha){
+        final int LIMCONECTION = 2;
+        final int QUANTMAXEADGE = 4;
+        int[] point = new int[QUANTMAXEADGE];
+        int p = LIMCONECTION+weight.nextInt(LIMCONECTION);
+        for(int i=0;i < p ;i++){
+            point[i] = (linha)+coord.nextInt(this.LIMITEEDGE[1]-linha);
         }
-        //point[0] = coord.nextInt(this.LIMITEWEIGHT[1]);
         return point;
     }
-    public static void main(String[] args) {
-        Maps mps = new Maps(2);
-        int[] rand = mps.coordGenerator();
-        System.out.println(rand[0]+","+rand[1]);
+    public void imprime(){
+        for(int i=0;i < this.adjacenciesMatrix.length;i++){
+            for(int j=0; j< this.adjacenciesMatrix.length;j++){
+                System.out.printf("%d, ", adjacenciesMatrix[i][j]);
+                if(j+1 == this.adjacenciesMatrix.length)
+                    System.out.printf("\n");
+            }
+        }
     }
-    
-    
+    public void dijkstra(int u, int v){
+        int w = u;
+        int[] gama = new int[this.size];
+        int[] beta = setbeta(this.size);
+        int[] pi = new int[this.size];
+        gama[u] = 1;
+        beta[u] = 0;
+    }
+    public int[] setbeta(int tamanho){
+        int[] gama = new int[tamanho];
+        int infinito = 999999999;
+        for(int i=0; i < tamanho;i++){
+            gama[i] = infinito;
+        }
+        return gama;
+    }
+    public Object edges(int vertice){
+        ArrayList<Integer> eadge = new ArrayList<Integer>();
+        for(int i=0;i < this.size;i++){
+            if (this.adjacenciesMatrix[vertice][i] != 0)
+                eadge.add(this.adjacenciesMatrix[vertice][i]);
+            if(this.adjacenciesMatrix[i][vertice] != 0)
+                eadge.add(this.adjacenciesMatrix[i][vertice]);
+        }
+        return eadge;
+    }
+    public static void main(String[] args) {
+        Maps mps = new Maps(10);
+        mps.imprime();
+        }
 }
